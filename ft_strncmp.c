2fa4add01502f2d8d7d34004771787da19702dfa
David Gallego Gomez <davgalle@c3r14s6.42madrid.com>
2023-09-30 18:49:58 +0200

    upload ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 16:35:05 by davgalle          #+#    #+#             */
-/*   Updated: 2023/09/19 15:30:29 by davgalle         ###   ########.fr       */
+/*   Updated: 2023/09/30 18:47:29 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,19 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n <= 0)
-		return (0);
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < (n -1))
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		i++;
+		if (s1[i] != s2[i])
+			return ((const unsigned char) s1[i] - (const unsigned char)s2[i]);
+		i ++;
 	}
-	if ((unsigned char)s1[i] < (unsigned char)s2[i])
-		return (-1);
-	else if ((unsigned char)s1[i] > (unsigned char)s2[i])
-		return (1);
-	else
-		return (0);
+	return (0);
 }
-/*
-int	main(void)
+/*int	main(void)
 {
-	char	s1[]= "eskerrik asko";
-	char	s2[]= "beti jai";
-	printf("%d", ft_strncmp(s1, s2, 5));
+	const char s1[]="abbcc";
+	const char s2[]="abbcd";
+	printf("%d", ft_strncmp(s1, s2, 4));
 
 	return (0);
 }*/
