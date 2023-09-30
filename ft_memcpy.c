e01e181f0f326f8cb8dc282be829b528af3aedaf
David Gallego Gomez <davgalle@c3r14s6.42madrid.com>
2023-09-30 20:36:17 +0200

    update ok

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:50:23 by davgalle          #+#    #+#             */
-/*   Updated: 2023/09/19 16:35:54 by davgalle         ###   ########.fr       */
+/*   Updated: 2023/09/30 20:31:01 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
-	size_t			i;
+	char		*d;
+	const char	*s;
+	size_t		i;
 
-	d = (unsigned char *)dst;
-	s = (unsigned char *)src;
+	d = dst;
+	s = src;
 	i = 0;
-	if (dst == NULL && src == NULL)
+	if (!s && !d)
 		return (NULL);
 	while (i < n)
 	{
