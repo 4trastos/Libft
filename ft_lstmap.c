27caa06370386c39ba9be8fc7818d3be6e79e492
David Gallego Gomez <davgalle@c1r11s2.42madrid.com>
2023-10-05 17:38:30 +0200

    new

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:31:48 by davgalle          #+#    #+#             */
-/*   Updated: 2023/09/29 17:33:32 by davgalle         ###   ########.fr       */
+/*   Updated: 2023/10/05 17:23:28 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,23 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	int		i;
-	t_list	*temp_lst;
 	t_list	*plst;
 	t_list	*plst_head;
 
 	i = 0;
-	temp_lst = lst;
 	plst_head = 0;
 	plst = 0;
-	if (lst == (void *)0)
-		return (0);
-	while (temp_lst != (void *)0)
+	while (lst != (void *)0)
 	{
-		plst = ft_lstnew((temp_lst->content));
+		plst = ft_lstnew((lst->content));
 		if (plst == 0)
 		{
 			ft_lstclear(&plst_head, del);
 			return (0);
 		}
-		plst->content = f(temp_lst->content);
+		plst->content = f(lst->content);
 		ft_lstadd_back(&plst_head, plst);
-		temp_lst = temp_lst->next;
+		lst = lst->next;
 		i++;
 	}
 	return (plst_head);
