0664fc573664f4d1aba834ff34e488fdbb4a54c2
David Gallego Gomez <davgalle@c1r11s2.42madrid.com>
2023-10-05 14:52:43 +0200

    upgrade

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:51:17 by davgalle          #+#    #+#             */
-/*   Updated: 2023/09/29 16:14:17 by davgalle         ###   ########.fr       */
+/*   Updated: 2023/10/05 14:45:20 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*aux;
 
-	if (*lst == (void *)0)
+	if (!*lst)
 	{
 		*lst = new;
+		new->next = NULL;
 	}
 	else
 	{
 		aux = *lst;
-		while (aux->next != (void *)0)
+		while (aux->next)
 			aux = aux->next;
 		aux->next = new;
 	}
