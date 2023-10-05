1d62369416d9cc5bca38d3ea3711e69fd9350372
David Gallego Gomez <davgalle@c1r11s2.42madrid.com>
2023-10-05 14:30:05 +0200

    upgrade

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:25:33 by davgalle          #+#    #+#             */
-/*   Updated: 2023/09/29 15:47:40 by davgalle         ###   ########.fr       */
+/*   Updated: 2023/10/05 14:23:51 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	t_list	*aux;
 
-	if (lst == (void *)0)
+	if (!lst)
 		return (lst);
 	aux = lst;
-	while (aux->next != (void *)0)
+	while (aux->next)
 		aux = aux->next;
 	return (aux);
 }
