80eba6bd9bd0d07172baf429e03a2f222052bd9e
David Gallego Gomez <davgalle@c1r11s2.42madrid.com>
2023-10-05 16:44:05 +0200

    upgrade

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 16:44:32 by davgalle          #+#    #+#             */
-/*   Updated: 2023/09/29 17:22:55 by davgalle         ###   ########.fr       */
+/*   Updated: 2023/10/05 16:37:59 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list	*temp;
 
 	temp = *lst;
-	if (*lst == (void *)0)
-		return ;
-	while (temp != (void *)0)
+	while (temp)
 	{
 		del (temp->content);
 		*lst = temp->next;
 		free (temp);
 		temp = *lst;
 	}
-	lst = (void *)0;
+	*lst = NULL;
 }
